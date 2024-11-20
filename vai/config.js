a502e081d3de888a0a7795d9261bfcacc9180dc7
Vgames1 <pooftheman2@gmail.com>
2024-11-20 18:00:59 +0200

    Update config.js

--- vai/config.js
+++ vai/config.js
@@ -1,1 +1,19 @@
-export const API_KEY = '9223ce87db2f21d05313414367963972f2b835fa';
+export const secureApiKey = '9223ce87db2f21d05313414367963972f2b835fa';
+
+export function getCurrentTime() {
+    return new Date().toLocaleTimeString();
+}
+
+export async function fetchImages(query) {
+    const response = await fetch('https://api.example.com/search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${secureApiKey}`
+        },
+        body: JSON.stringify({ query })
+    });
+
+    const data = await response.json();
+    return data.images[0].url;
+}
